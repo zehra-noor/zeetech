bd0431e1512e1cba84aad7c732e056416391e824
Zehra Noor <zehra.noor92@gmail.com>
2023-09-18 18:37:53 +0500

    vercel.jso updated

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,10 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //uploads
+  app.use("/uploads", express.static("/var/data/uploads"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
